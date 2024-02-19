a640757b8710c5c22bf1ebe0876dc1d3113d64ea
Marcus Rådell <marcus@radell.net>
2024-02-19 09:51:46 +0100

    remove main as it didn't do anything

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import { main } from "./main";
+import { createAccount } from "./account";
 
 // Empty email throws an error.
-main("38041b2a-30a7-4601-bee3-08e905d8b093", "");
+try {
+  const account = createAccount("38041b2a-30a7-4601-bee3-08e905d8b093", "");
+  console.log(account);
+} catch (error) {
+  console.error(error);
+}
