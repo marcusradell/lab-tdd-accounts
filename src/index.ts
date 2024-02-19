4950c7588a2bb062b9db95d1aba8fface18aa809
Marcus Rådell <marcus@radell.net>
2024-02-19 09:42:42 +0100

    handle empty email

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, world!");
+type CreateAccount = (id: string, email: string) => void;
+
+const createAccount: CreateAccount = (id, email) => {
+  if (email === "") {
+    throw new Error("Email is required.");
+  }
+
+  throw new Error("Not implemented");
+};
+
+const main = () => {
+  try {
+    const account = createAccount("38041b2a-30a7-4601-bee3-08e905d8b093", "");
+    console.log(account);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();
